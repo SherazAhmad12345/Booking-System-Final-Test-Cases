1f16bac8d7151464f707ec88bdd02f9adfaba886
eychao-027 <eychao027@gmail.com>
2026-07-27 23:13:27 -0400

    User Test Cases
    From previous version?
    Just added one more test

---
+++ BookingSystem/src/manualTests/UserFactoryManualTest.java
@@ -0,0 +1,77 @@
+package manualTests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import userManagement.User;
+import userManagement.UserFactory;
+
+public class UserFactoryManualTest {
+
+	User student;
+	User faculty;
+	User other;
+	User partner;
+	User staff;
+	User unknown;
+
+	@Before
+	public void setup() {
+		student = UserFactory.createUser("Student", "username1", "password1", "email1", 1, "1");
+		faculty = UserFactory.createUser("Faculty", "username2", "password2", "email2", 2, "2");
+		other = UserFactory.createUser("Other", "username3", "password3", "email3", 3, "3");
+		partner = UserFactory.createUser("Partner", "username4", "password4", "email4", 4, "4");
+		staff = UserFactory.createUser("Staff", "username5", "password5", "email5", 5, "5");
+
+	}
+
+	@Test
+	public void createUserTest() {
+		assertEquals("Incorrect user type.", student.getUserType(), "Student");
+		assertEquals("Incorrect user type.", faculty.getUserType(), "Faculty");
+		assertEquals("Incorrect user type.", other.getUserType(), "Other");
+		assertEquals("Incorrect user type.", partner.getUserType(), "Partner");
+		assertEquals("Incorrect user type.", staff.getUserType(), "Staff");
+		assertThrows(IllegalArgumentException.class, () -> {
+			unknown = UserFactory.createUser("unknown", "username5", "password5", "email5", 5, "5");
+			;
+		});
+		assertThrows(IllegalArgumentException.class, () -> {
+			unknown = UserFactory.createUser(null, "username5", "password5", "email5", 5, "5");
+			;
+		});
+	}
+
+	@Test
+	public void isStrongPassword() {
+		assertFalse("Returned true despite password being weak.", UserFactory.isStrongPassword("s"));
+		assertFalse("Returned true despite password being weak.", UserFactory.isStrongPassword("sherazahmad"));
+		assertFalse("Returned true despite password being weak.", UserFactory.isStrongPassword("sherazahmad1"));
+		assertFalse("Returned true despite password being weak.", UserFactory.isStrongPassword("sheraz@ahmad1"));
+		assertTrue("Returned false despite password being strong.", UserFactory.isStrongPassword("Sheraz@ahmad1"));
+	}
+
+	@Test
+	public void isValidEmail() {
+		assertFalse("Returned true despite having empty email input.", UserFactory.isValidEmail("", "Other"));
+		assertFalse("Returned true despite having null email input.", UserFactory.isValidEmail(null, "Other"));
+		assertFalse("Returned true despite missing required @my.yorku.ca.", UserFactory.isValidEmail("orku.ca", "Student"));
+		assertFalse("Returned true despite missing required text before @my.yorku.ca.", UserFactory.isValidEmail("@my.yorku.ca", "Student"));
+		assertTrue("Returned false despite meeting the format requirments for student emails.", UserFactory.isValidEmail("sha@my.yorku.ca", "Student"));
+		assertFalse("Returned true despite missing required @yorku.ca.", UserFactory.isValidEmail("orku.ca", "Faculty"));
+		assertFalse("Returned true despite missing required text before @yorku.ca.", UserFactory.isValidEmail("@yorku.ca", "Faculty"));
+		assertTrue("Returned false despite meeting the format requirements for faculty emails.", UserFactory.isValidEmail("sha@yorku.ca", "Faculty"));
+		assertFalse("Returned true despite missing required @yorku.ca.", UserFactory.isValidEmail("orku.ca", "Staff"));
+		assertFalse("Returned true despite missing required text before @yorku.ca", UserFactory.isValidEmail("@yorku.ca", "Staff"));
+		assertTrue("Returned false despite meeting the format requirements for staff emails.", UserFactory.isValidEmail("sha@yorku.ca", "Staff"));
+		assertFalse("Returned true despite missing text before the @ symbol.", UserFactory.isValidEmail("@gmail.com", "Partner"));
+		assertFalse("Returned true despite missing a . after the @ symbol.", UserFactory.isValidEmail("nam.e@gmailcom", "Partner"));
+		assertFalse("Returned true despite having a . as the last symbol in the string input.", UserFactory.isValidEmail("name@gmail.com.", "Partner"));
+		assertTrue("Returned false despite meeting all non-university email requirments.", UserFactory.isValidEmail("name@gmail.com", "Partner"));
+	}
+}
---
+++ BookingSystem/src/manualTests/UserTypeManualTest.java
@@ -0,0 +1,156 @@
+package manualTests;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import userManagement.Faculty;
+import userManagement.Other;
+import userManagement.Partner;
+import userManagement.Staff;
+import userManagement.Student;
+import userManagement.User;
+
+public class UserTypeManualTest {
+
+	User faculty;
+	User student;
+	User staff;
+	User partner;
+	User other;
+
+	@Before
+	public void setup() {
+		faculty = new Faculty("username1", "password1", "email1", 0, "0");
+		student = new Student("username2", "password2", "email2", 1, "1");
+		staff = new Staff("username3", "password3", "email3", 2, "2");
+		partner = new Partner("username4", "password4", "email4", 3, "3");
+		other = new Other("username5", "password5", "email5", 4, "4");
+	}
+
+	@Test
+	public void testGetters() {
+		assertTrue("getUsername() did not return expected result for faculty.", faculty.getUsername() == "username1");
+		assertTrue("getUsername() did not return expected result for student.", student.getUsername() == "username2");
+		assertTrue("getUsername() did not return expected result for staff.", staff.getUsername() == "username3");
+		assertTrue("getUsername() did not return expected result for partner.", partner.getUsername() == "username4");
+		assertTrue("getUsername() did not return expected result for other.", other.getUsername() == "username5");
+
+		assertTrue("getPassword() did not return expected result for faculty.", faculty.getPassword() == "password1");
+		assertTrue("getPassword() did not return expected result for student.", student.getPassword() == "password2");
+		assertTrue("getPassword() did not return expected result for staff.", staff.getPassword() == "password3");
+		assertTrue("getPassword() did not return expected result for partner.", partner.getPassword() == "password4");
+		assertTrue("getPassword() did not return expected result for other.", other.getPassword() == "password5");
+
+		assertTrue("getEmail() did not return expected result for faculty.", faculty.getEmail() == "email1");
+		assertTrue("getEmail() did not return expected result for student.", student.getEmail() == "email2");
+		assertTrue("getEmail() did not return expected result for staff.", staff.getEmail() == "email3");
+		assertTrue("getEmail() did not return expected result for partner.", partner.getEmail() == "email4");
+		assertTrue("getEmail() did not return expected result for other.", other.getEmail() == "email5");
+
+		assertTrue("getID() did not return expected result for faculty.", faculty.getId() == 0);
+		assertTrue("getID() did not return expected result for student.", student.getId() == 1);
+		assertTrue("getID() did not return expected result for staff.", staff.getId() == 2);
+		assertTrue("getID() did not return expected result for partner.", partner.getId() == 3);
+		assertTrue("getID() did not return expected result for other.", other.getId() == 4);
+
+		assertTrue("getOrgIdOrStudentNumber() did not return expected result for faculty.",
+				faculty.getOrgIdOrStudentNumber() == "0");
+		assertTrue("getOrgIdOrStudentNumber() did not return expected result for student.",
+				student.getOrgIdOrStudentNumber() == "1");
+		assertTrue("getOrgIdOrStudentNumber() did not return expected result for staff.",
+				staff.getOrgIdOrStudentNumber() == "2");
+		assertTrue("getOrgIdOrStudentNumber() did not return expected result for partner.",
+				partner.getOrgIdOrStudentNumber() == "3");
+		assertTrue("getOrgIdOrStudentNumber() did not return expected result for other.",
+				other.getOrgIdOrStudentNumber() == "4");
+
+		assertTrue("getHourlyRate() did not return expected result for faculty.", faculty.getHourlyRate() == 30);
+		assertTrue("getHourlyRate() did not return expected result for student.", student.getHourlyRate() == 20);
+		assertTrue("getHourlyRate() did not return expected result for staff.", staff.getHourlyRate() == 40);
+		assertTrue("getHourlyRate() did not return expected result for partner.", partner.getHourlyRate() == 50);
+		assertTrue("getID() did not return expected result for other.", other.getHourlyRate() == 50);
+
+		assertTrue("getUserType() did not return expected result for faculty.", faculty.getUserType() == "Faculty");
+		assertTrue("getUserType() did not return expected result for student.", student.getUserType() == "Student");
+		assertTrue("getUserType() did not return expected result for staff.", staff.getUserType() == "Staff");
+		assertTrue("getUserType() did not return expected result for partner.", partner.getUserType() == "Partner");
+		assertTrue("getUserType() did not return expected result for other.", other.getUserType() == "Other");
+	}
+
+	@Test
+	public void testSetters() {
+		faculty.setEmail("email10");
+		faculty.setPassword("password10");
+		faculty.setUsername("username10");
+		faculty.setId(10);
+		faculty.setOrgIdOrStudentNumber("10");
+
+		student.setEmail("email11");
+		student.setPassword("password11");
+		student.setUsername("username11");
+		student.setId(11);
+		student.setOrgIdOrStudentNumber("11");
+		
+		staff.setEmail("email12");
+		staff.setPassword("password12");
+		staff.setUsername("username12");
+		staff.setId(12);
+		staff.setOrgIdOrStudentNumber("12");
+
+		partner.setEmail("email13");
+		partner.setPassword("password13");
+		partner.setUsername("username13");
+		partner.setId(13);
+		partner.setOrgIdOrStudentNumber("13");
+
+		other.setEmail("email14");
+		other.setPassword("password14");
+		other.setUsername("username14");
+		other.setId(14);
+		other.setOrgIdOrStudentNumber("14");
+
+		assertTrue("getUsername() did not return expected result for faculty.", faculty.getUsername() == "username10");
+		assertTrue("getUsername() did not return expected result for student.", student.getUsername() == "username11");
+		assertTrue("getUsername() did not return expected result for staff.", staff.getUsername() == "username12");
+		assertTrue("getUsername() did not return expected result for partner.", partner.getUsername() == "username13");
+		assertTrue("getUsername() did not return expected result for other.", other.getUsername() == "username14");
+
+		assertTrue("getPassword() did not return expected result for faculty.", faculty.getPassword() == "password10");
+		assertTrue("getPassword() did not return expected result for student.", student.getPassword() == "password11");
+		assertTrue("getPassword() did not return expected result for staff.", staff.getPassword() == "password12");
+		assertTrue("getPassword() did not return expected result for partner.", partner.getPassword() == "password13");
+		assertTrue("getPassword() did not return expected result for other.", other.getPassword() == "password14");
+
+		assertTrue("getEmail() did not return expected result for faculty.", faculty.getEmail() == "email10");
+		assertTrue("getEmail() did not return expected result for student.", student.getEmail() == "email11");
+		assertTrue("getEmail() did not return expected result for staff.", staff.getEmail() == "email12");
+		assertTrue("getEmail() did not return expected result for partner.", partner.getEmail() == "email13");
+		assertTrue("getEmail() did not return expected result for other.", other.getEmail() == "email14");
+
+		assertTrue("getID() did not return expected result for faculty.", faculty.getId() == 10);
+		assertTrue("getID() did not return expected result for student.", student.getId() == 11);
+		assertTrue("getID() did not return expected result for staff.", staff.getId() == 12);
+		assertTrue("getID() did not return expected result for partner.", partner.getId() == 13);
+		assertTrue("getID() did not return expected result for other.", other.getId() == 14);
+
+		assertTrue("getOrgIdOrStudentNumber() did not return expected result for faculty.",
+				faculty.getOrgIdOrStudentNumber() == "10");
+		assertTrue("getOrgIdOrStudentNumber() did not return expected result for student.",
+				student.getOrgIdOrStudentNumber() == "11");
+		assertTrue("getOrgIdOrStudentNumber() did not return expected result for staff.",
+				staff.getOrgIdOrStudentNumber() == "12");
+		assertTrue("getOrgIdOrStudentNumber() did not return expected result for partner.",
+				partner.getOrgIdOrStudentNumber() == "13");
+		assertTrue("getOrgIdOrStudentNumber() did not return expected result for other.",
+				other.getOrgIdOrStudentNumber() == "14");
+
+	}
+
+	@Test
+	public void toStringTest() {
+       assertEquals(student.toString(), "Student" + " [username=" + "username2" + ", email=" + "email2" + ", id=1]", "toString() method does not produce expected output");
+	}
+}
