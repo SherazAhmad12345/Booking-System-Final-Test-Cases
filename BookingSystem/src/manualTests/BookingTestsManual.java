887ca11bd8b8e5d28ff77cbef2987df5dc908d65
Sheraz Ahmad <119704391+SherazAhmad12345@users.noreply.github.com>
2026-07-28 01:11:02 -0400

    Add files via upload

--- BookingSystem/src/manualTests/BookingTestsManual.java
+++ BookingSystem/src/manualTests/BookingTestsManual.java
@@ -1,10 +1,14 @@
 package manualTests;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.text.SimpleDateFormat;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
 import java.util.Date;
 
 import org.junit.Before;
@@ -17,27 +21,66 @@ import roomManagement.RoomManager;
 public class BookingTestsManual {
 
 	Booking booking;
+	Booking booking2;
+	Booking booking3;
 	Date date;
+	String end1;
+	String end2;
+	String start1;
+	String start2;
+	String start3;
 
 	@Before
 	public void setUp() {
-		date = new Date();
-		booking = new Booking("userID", "roomID", "bookingID", date, "3:30", "3:50", 30, 30, 30);
+		ZonedDateTime anchor = ZonedDateTime.now().withHour(12).withMinute(0).withSecond(0).withNano(0);
+		date = Date.from(anchor.toInstant());
+		ZonedDateTime startTime1 = anchor.minusMinutes(10);
+		ZonedDateTime startTime2 = anchor.minusMinutes(60);
+		ZonedDateTime startTime3 = anchor.plusMinutes(20);
+		ZonedDateTime endTime1 = anchor.plusMinutes(30);
+		ZonedDateTime endTime2 = anchor.minusMinutes(10);
+		end1 = endTime1.format(DateTimeFormatter.ofPattern("HH:mm"));
+		end2 = endTime2.format(DateTimeFormatter.ofPattern("HH:mm"));
+		start1 = startTime1.format(DateTimeFormatter.ofPattern("HH:mm"));
+		start2 = startTime2.format(DateTimeFormatter.ofPattern("HH:mm"));
+		start3 = startTime3.format(DateTimeFormatter.ofPattern("HH:mm"));
+		booking = new Booking("userID", "roomID", "bookingID", date, start1, end1, 30, 30, 30);
+		booking2 = new Booking("userID2", "roomID2", "bookingID2", date, start2, end2, 30, 30, 30);
+		booking3 = new Booking("userID3", "roomID3", "bookingID3", date, start3, end1, 30, 30, 30);
 	}
 	
-	//TODO: Please refer to D3 Document to see what test cases need to be added, and fix the other TODO's below. 
-
 	@Test
 	public void testGetters() {
 		boolean isExpected = false;
 
 		if (booking.getUserID() == "userID" && booking.getRoomID() == "roomID" && booking.getBookingID() == "bookingID"
-				&& booking.getDate() == date && booking.getStartTime() == "3:30" && booking.getEndTime() == "3:50"
+				&& booking.getDate() == date && booking.getStartTime() == start1 && booking.getEndTime() == end1
 				&& booking.getDepositPaid() == 30 && booking.getHourlyRate() == 30 && booking.getTotalCost() == 30) {
 			isExpected = true;
 		}
 
 		assertTrue("Getters did not return expected values.", isExpected);
+	}
+	
+	@Test
+	public void testEndTime()
+	{
+		booking.setEndTime("14:30");
+		assertEquals("End time was not updated after using the setter method.", booking.getEndTime(), "14:30");
+	}
+
+	@Test
+	public void isActiveNow() {
+		assertTrue("Returned false despite booking being currently active.", booking.isActiveAt(date));
+		assertFalse("Returned true despite booking having ended.", booking2.isActiveAt(date));
+		assertFalse("Returned true despite booking having not started.", booking3.isActiveAt(date));
+	}
+
+	@Test
+	public void isDepositForfeitedTest() {
+		assertTrue("Returned false despite session starting over 30 minutes ago.", booking2.isDepositForfeited(date));
+		booking2.setCheckedIn(true);
+		assertFalse("Returned true despite already being checked in.", booking2.isDepositForfeited(date));
 	}
 
 	@Test
@@ -51,26 +94,51 @@ public class BookingTestsManual {
 			fail("checked in status is not set to false by default");
 		}
 	}
-	
+
 	@Test
-	public void combineDateAndTimeTest() 
-	{
-		//TODO: Implement
+	public void hasStartedTest() {
+
+		assertTrue("Returned false despite having already started.", booking.hasStarted(date));
+		assertFalse("Returned true despite not starting yet.", booking3.hasStarted(date));
+
 	}
-	
-	@Test 
-	public void toStringTest()
-	{
-		String base = "roomID" + " on " + new SimpleDateFormat("MM/dd/yy").format(date) + " " + "3: 30" + "-" + "3: 50";
+
+	@Test
+	public void hasEndedTest() {
+
+		assertTrue("Returned false despite having already ended.", booking2.hasExpired(date));
+		assertFalse("Returned true despite having not ended.", booking.hasExpired(date));
+	}
+
+	@Test
+	public void combineDateAndTimeTest() {
+		Date inputDate = new Date();
+
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(inputDate);
+		cal.set(Calendar.HOUR_OF_DAY, 14);
+		cal.set(Calendar.MINUTE, 30);
+		cal.set(Calendar.SECOND, 0);
+		cal.set(Calendar.MILLISECOND, 0);
+		Date expectedDate = cal.getTime();
+		Date actualDate = Booking.combineDateAndTime(inputDate, "14:30");
+		assertEquals(expectedDate, actualDate);
+	}
+
+	@Test
+	public void toStringTest() {
+		String base1 = "roomID" + " on " + new SimpleDateFormat("MM/dd/yy").format(date) + " " + booking.getStartTime() + "-" + booking.getEndTime();
 		String status1 = " [checked in]";
 		String status2 = " [deposit lost]";
-		
-	    booking.setDate(new Date()); //TODO: Adjust Appropriately.	
-		assertEquals("Does not match expected output", booking.toString(), base);
-		booking.setCheckedIn(true);	
-		assertEquals("Does not match expected output", booking.toString(), base + status1);
+		assertEquals("Does not match expected output.", booking.toString(), base1);
+		booking.setCheckedIn(true);
+		assertEquals("Does not match expected output.", booking.toString(), base1 + status1);
 		booking.setCheckedIn(false);
-		booking.setDate(new Date()); //TODO: Adjust Appropriately.	
-		assertEquals("Does not match expected output", booking.toString(), base + status2);
+		Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.DATE, -1);
+		Date yesterday = cal.getTime();
+		booking2.setDate(yesterday);
+		String base2 = "roomID2" + " on " + new SimpleDateFormat("MM/dd/yy").format(yesterday) + " " + booking2.getStartTime() + "-" + booking2.getEndTime();
+		assertEquals("Does not match expected output.", booking2.toString(), base2 + status2);
 	}
 }
---
+++ BookingSystem/src/manualTests/ScannerManualTests.java
@@ -0,0 +1,79 @@
+package manualTests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.Test;
+
+import bookingManagment.BookingManager;
+import roomManagement.RoomManager;
+import roomStatus.ClosedState;
+import roomStatus.DisabledState;
+import roomStatus.EnabledState;
+import roomAccess.Scanner;
+
+public class ScannerManualTests {
+
+	Scanner scanner;
+	Scanner scanner1;
+	static RoomManager rm;
+
+	@Before
+	public void setup() {
+		rm = RoomManager.getRoomManagerInstance();
+		rm.addRoom("DB", 2, "Data Lab");
+		rm.addRoom("DB1", 2, "Data Lab1");
+		scanner = new Scanner("scannerID", rm.selectRoomID("DB"));
+		scanner1 = new Scanner("scannerID1", rm.selectRoomID("DB1"));
+	}
+
+	@AfterClass
+	public static void resetRooms() {
+		rm.resetRooms();
+	}
+
+	// Note, scanBadgeTest already tests verifyUserID() which is called by the
+	// scanBadge() method.
+
+	@Test
+	public void scanBadgeTest() {
+		ZonedDateTime now = ZonedDateTime.now();
+		ZonedDateTime startOfDay = now.toLocalDate().atStartOfDay(now.getZone());
+		ZonedDateTime endOfDay = startOfDay.plusDays(1).minusMinutes(1); // 23:59 today
+
+		ZonedDateTime startTime1 = now.minusMinutes(1).isBefore(startOfDay) ? startOfDay : now.minusMinutes(1);
+		ZonedDateTime endTime = now.plusMinutes(30).isAfter(endOfDay) ? endOfDay : now.plusMinutes(30);
+		ZonedDateTime startTime2 = now.plusMinutes(20).isAfter(endOfDay) ? endOfDay : now.plusMinutes(20);
+
+		String end = endTime.format(DateTimeFormatter.ofPattern("HH:mm"));
+		String start1 = startTime1.format(DateTimeFormatter.ofPattern("HH:mm"));
+		String start2 = startTime2.format(DateTimeFormatter.ofPattern("HH:mm"));
+
+		Date today = new Date();
+		BookingManager.getInstance().createBooking("UserID", "DB", "BookingID", today, start1,
+				end, 0, 0, 0);
+		BookingManager.getInstance().createBooking("UserID1", "DB1", "BookingID1", today, start2, end, 0, 0,
+				0);
+		assertEquals("scanBadge() returns false when expected true.", "accessGranted", scanner.scanBadge("UserID"));
+		assertEquals("scanBadge() returns true when expected false.", "accessDenied", scanner1.scanBadge("UserID1"));
+		assertEquals("scanBadge() returns true when expected false.", "accessDenied", scanner.scanBadge("UserID2"));
+	}
+	
+	@Test
+	public void requestEntryTest() {
+		rm.selectRoomID("DB").setState(new ClosedState());
+		assertFalse("Does not deny entry if room is closed.", scanner.requestEntry("UserID"));
+		rm.selectRoomID("DB").setState(new DisabledState());
+		assertFalse("Does not deny entry if room is disabled.", scanner.requestEntry("UserID"));
+		rm.selectRoomID("DB").setState(new EnabledState());
+		assertTrue("Does not allow entry if room is enabled.", scanner.requestEntry("UserID"));
+	}
+}
